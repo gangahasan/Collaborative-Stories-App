9530d0a84fa828cdb105f719ad743249f14750f7
gangahasan <rgangabhavani2@gmail.com>
2025-03-05 15:49:10 +0530

    bug fix at email

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     console.log(userdata,"data")
   return (
     <Container>
-        <Heading size="2xl" textAlign="center" padding="8">Welcome   <span style={{color:"blue"}}>{userdata.email}</span></Heading>
+        <Heading size="2xl" textAlign="center" padding="8">Welcome   <span style={{color:"blue"}}>{userdata?.email}</span></Heading>
         
         <h2>Recent Posts</h2>
         
